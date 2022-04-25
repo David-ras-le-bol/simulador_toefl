8d9e0eed163cdbef6d66abccaa1f2551b0e085f2
Ale <reservaale@gmail.com>
2022-04-25 17:49:34 -0500

    Recuperar contraseña

--- controllers/usuarios.controller.php
+++ controllers/usuarios.controller.php
@@ -1,5 +1,14 @@
 <?php
 
+    use  PHPMailer \ PHPMailer \ PHPMailer ;
+    use  PHPMailer \ PHPMailer \ Exception ;
+
+    require 'controllers/PHPMailer /Exception.php' ;
+    require 'controllers/PHPMailer /PHPMailer.php' ;
+    require 'controllers/PHPMailer /SMTP.php' ;
+
+
+
 class UsuariosController {
 
     // ======================
@@ -8,14 +17,14 @@ class UsuariosController {
 
     public static function registroUsuarioCtr(){
 
-        if(isset($_POST["regNombre"])){
+        if(isset($_POST["nombre"])){
 
             $tabla = "usuarios";
 
-            $datos = array("nombre"=>$_POST["regNombre"],
-                            "apellido"=>$_POST["regApellido"],
-                            "email"=>$_POST["regEmail"],
-                            "contraseña"=>$_POST["regPassword"]);
+            $datos = array("nombre"=>$_POST["nombre"],
+                            "apellido"=>$_POST["apellido"],
+                            "email"=>$_POST["email"],
+                            "contraseña"=>$_POST["password"]);
 
             $respuesta = UsuariosModel::registroUsuarioMdl($datos, $tabla);
 
@@ -53,65 +62,128 @@ class UsuariosController {
         }
         
     }
+    
+    
+    
+      // ======================
+    // OLVIDE CONTRASEÑA
+    // ======================
 
-    /*======================================================
-        MOSTRAR USUARIO
-    ======================================================*/
-    public static function ctrMostrarUsuario($item, $valor){
+    
+    public static function ctrOlvidoPassword(){
+    
+    
+   
+        
+        
+        
+        
+     if(isset($_POST["pass_email"])){
+       
+        //GENERAR CONTRASEÑA ALEATORIA
+         
+         function Crear_clave(){
 
-        $tabla = "usuarios";
+            //Obtengo numeros 
+             
+            $num=mt_rand(100,900);
 
-        $respuesta = UsuariosModel::mdlMostrarUsuario($tabla, $item, $valor);
+            //Obtengo letras
+             
+            $letra=  chr(rand(ord("A"), ord("Z")));
 
-        return $respuesta;
+            $clave=$num . $letra;
 
-    }
+            //echo "La clave es ".$clave; 
+            return $clave;
+        }
+     
+     $new_password=Crear_clave();
+     $tabla="usuarios";
+     $item1="email";
+     $valor1=$_POST["pass_email"];
+     
+         
+     $respuesta1=UsuariosModel::mdlMostrarUsuario($tabla,$item1,$valor1);
+         
+        if($respuesta1){
+            $id=$respuesta1["id"];
+            $item2="password";
+            $valor2=$new_password;
+             
+            $respuesta2=UsuariosModel::mdlOlvidoPassword($tabla,$id,$item2,$valor2);
+            
+            if($respuesta2=="ok"){
+                
+                //1. Destino
+                $destinatario=$valor1;
 
-    /*======================================================
-        INGRESO USUARIO 91
-    ======================================================*/
-    public static function ingresoUsuarioCtr(){
 
-        if(isset($_POST["ingEmail"])){
+                //3.Enviar mensaje
 
-            $password = $_POST["ingPassword"];
+                $mail = new PHPMailer(true);
 
-            $tabla = "usuarios";
-            $item = "email";
-            $valor = $_POST["ingEmail"];
+                try {
+                //Server settings
+                $mail->SMTPDebug = 2;                                     //Muestra errores
+                $mail->isSMTP();                                          //SMTP
+                $mail->Host       = 'smtp.gmail.com';                     //Servidor de correo utilizado
+                $mail->SMTPAuth   = true;                                  //SMTP
+                $mail->Username   = 'simToefl@gmail.com';                  //correo origen
+                $mail->Password   = 'p123toefl';                          //contraseña del correo origen
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;        //Seguridad 
+                $mail->Port       = 587;                                   //Puerto TCP
 
-            $respuesta = UsuariosModel::mdlMostrarUsuario($tabla, $item, $valor);
+                //Correos electronicos a utilizar
+                $mail->setFrom('simToefl@gmail.com', 'SIMULADOR TOEFL:RECUPERACION DE PASSWORD'); //Correo origen
+                $mail->addAddress($destinatario);     //Correo destino
 
-            if($respuesta["email"] == $_POST["ingEmail"] && $respuesta["password"] == $password){
 
-                $_SESSION["validarSesion"] = "ok";
-                $_SESSION["id"] = $respuesta["id"];
-                $_SESSION["nombre"] = $respuesta["nombre"];
-                $_SESSION["foto"] = $respuesta["foto"];
-                $_SESSION["email"] = $respuesta["email"];
-                $_SESSION["password"] = $respuesta["password"];
+                //Datos del archivo
+                $mail->isHTML(true);                                  //Acepta archivos con formato HTML
+                $mail->Subject = 'Recuperacion de password';           //Asunto
+                $mail->Body    = 'La contraseña temporal asignada a su cuenta es  '.$valor2.'  cambiela al ingresar al sistema.';  //Mensaje 
 
-                echo '<script>
 
-                    window.location = "http://localhost/simulador_toefl/";
+                $mail->send();                                        //Envia mensaje
 
-                </script>';
-
-            }else{
-
-                echo '<script>
+                     echo '<script>
                     Swal.fire({
-                        icon: "error",
-                        title: "Error al Ingresar",
-                        text: "El email o la contraseña son incorrectos",
-                        confirmButtonText: "ok"
+                        icon: "success",
+                        title: "Revise su correo, se le ha enviado su contraseña temporal",
+                        confirmButtonText: "Ok"
+                    }).then((result) => {
+                        if(result.isConfirmed){
+                            window.location.href = "http://localhost/simulador_toefl/login";
+                        }
                     })
                 </script>';
 
-            }
+            } catch (Exception $e) {                                   //Si falla el envio del msj. muestra mensaje
+                echo "No se envio el email : {$mail->ErrorInfo}";}
 
+                
+                  
+        }else{
+            
+             echo '<script>
+                    Swal.fire({
+                        icon: "error",
+                        title: "El correo no existe"
+                    }) 
+                </script>';
+
+            
+            
         }
 
+     
     }
+    
+    
+    
 
 }
+}
+    
+}//CLASE
--- index.php
+++ index.php
@@ -2,12 +2,20 @@
 
 require_once "controllers/template.controller.php";
 require_once "controllers/usuarios.controller.php";
-require_once "controllers/password.controller.php";
 
 require_once "models/template.model.php";
 require_once "models/usuarios.model.php";
 
 require_once "models/routes.php";
+
+
+use  PHPMailer \ PHPMailer \ PHPMailer ;
+use  PHPMailer \ PHPMailer \ Exception ;
+
+require_once 'controllers/PHPMailer /Exception.php' ;
+require_once 'controllers/PHPMailer /PHPMailer.php' ;
+require_once 'controllers/PHPMailer /SMTP.php' ;
+    
 
 $template = new ControllerTemplate();
 $template -> template();
