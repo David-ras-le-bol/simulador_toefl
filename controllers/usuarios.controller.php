9a24209ad8b0be9637b981c3714e3c8a4e471eaa
Wicho Canul <wichocanul07@gmail.com>
2022-04-26 00:42:59 -0500

    se agrego la funcion de cambio de contraseña por phpmailer

--- controllers/usuarios.controller.php
+++ controllers/usuarios.controller.php
@@ -1,7 +1,7 @@
 <?php
 
-    use  PHPMailer\PHPMailer\PHPMailer;
-    use  PHPMailer\PHPMailer\Exception;
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
 
     require 'controllers/PHPMailer /Exception.php' ;
     require 'controllers/PHPMailer /PHPMailer.php' ;
